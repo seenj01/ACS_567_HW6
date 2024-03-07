fc58f814a1fa46bb3ab133cba3e4b6648e86e85f
seenj01 <seenj01@pfw.edu>
2024-03-07 00:09:02 -0500

    updated velocity calculation code

--- velocity.py
+++ velocity.py
@@ -1,3 +1,6 @@
+def calculate_average_velocity(sprint_pts_list):
+    return sum(sprint_pts_list) / len(sprint_pts_list)
+
 print("\nAverage Velocity Calculator")
 
 sprint_cnt = int(input("Enter number of sprints to consider: "))
@@ -8,6 +11,6 @@ for i in range(sprint_cnt):
     pts = int(input(f"Enter the total points completed during sprint {i}: "))
     sprint_pts_list.append(pts)
 
-avg_velocity = sum(sprint_pts_list) / sprint_cnt
+avg_velocity = calculate_average_velocity(sprint_pts_list)
 
 print(f"\nAverage team velocity for {sprint_cnt} sprints = {avg_velocity}\n")

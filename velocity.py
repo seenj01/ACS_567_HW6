c86b14bee6d2eac582ebedf38948a904229259e2
seenj01 <seenj01@pfw.edu>
2024-03-16 10:54:36 -0400

    test file added

--- velocity.py
+++ velocity.py
@@ -1,16 +1,20 @@
 def calculate_average_velocity(sprint_pts_list):
+    if any(pt < 0 for pt in sprint_pts_list):
+        raise ValueError("Sprint points cannot be negative")
+    if not sprint_pts_list:
+        return 0
     return sum(sprint_pts_list) / len(sprint_pts_list)
 
-print("\nAverage Velocity Calculator")
 
-sprint_cnt = int(input("Enter number of sprints to consider: "))
+if __name__ == "__main__":
+    sprint_cnt = int(input("Enter number of sprints to consider: "))
 
-sprint_pts_list = []
+    sprint_pts_list = []
 
-for i in range(sprint_cnt):
-    pts = int(input(f"Enter the total points completed during sprint {i}: "))
-    sprint_pts_list.append(pts)
+    for i in range(sprint_cnt):
+        pts = int(input(f"Enter the total points completed during sprint {i}: "))
+        sprint_pts_list.append(pts)
 
-avg_velocity = calculate_average_velocity(sprint_pts_list)
+    avg_velocity = calculate_average_velocity(sprint_pts_list)
 
-print(f"\nAverage team velocity for {sprint_cnt} sprints = {avg_velocity}\n")
+    print(f"\nAverage team velocity for {sprint_cnt} sprints = {avg_velocity}\n")
